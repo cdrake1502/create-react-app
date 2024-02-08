67dfd0d29da5fa96b7183de6175730fb6cd83a8b
elifew <elifew@users.noreply.github.com>
2024-02-08 12:39:51 -0500

    splash css fix and db creation template

---
+++ src/api/create-login-table/route.js
@@ -0,0 +1,12 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request) {
+  try {
+    const result =
+      await sql`CREATE TABLE Login ( FirstName varchar(255), LastName varchar(255), Email varchar(255), Password varchar(255));`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
