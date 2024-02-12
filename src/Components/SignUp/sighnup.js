5837089becaf124e85158b5dfcf21012bd9b6fb7
elifew <elifew@users.noreply.github.com>
2024-02-12 13:25:55 -0500

    Update sighnup.js

--- src/Components/SignUp/sighnup.js
+++ src/Components/SignUp/sighnup.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import './signup_global.css';
 import './signup_index.css';
 import {createClient} from '@supabase/supabase-js';
-const supbase = createClient('https://vyvojvrtkryvbsmcgzrq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2OTczNjYsImV4cCI6MjAyMzI3MzM2Nn0.g0vq8MRsOox8wKVEq8gde9CA_2egvpfMfoqDHiWJxv4');
+const supabase = createClient('https://vyvojvrtkryvbsmcgzrq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2OTczNjYsImV4cCI6MjAyMzI3MzM2Nn0.g0vq8MRsOox8wKVEq8gde9CA_2egvpfMfoqDHiWJxv4');
 
 
 
@@ -14,47 +14,45 @@ const SignUp =() =>{
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
   
-    const handleSubmit = (e) => {
+    const handleSubmit = async function (e) {
       e.preventDefault();
-  
+
       // Regular expressions for password complexity
       const numberRegex = /\d/;
       const uppercaseRegex = /[A-Z]/;
       const lowercaseRegex = /[a-z]/;
       const specialCharRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]/;
-  
+
       // Perform validation checks
-      if (
-        !email.includes('@') ||
+      if (!email.includes('@') ||
         password.length < 6 ||
         !numberRegex.test(password) ||
         !uppercaseRegex.test(password) ||
         !lowercaseRegex.test(password) ||
-        !specialCharRegex.test(password)
-      ) {
+        !specialCharRegex.test(password)) {
         setError(
           'Password must be at least 6 characters long and include at least one number, one uppercase letter, one lowercase letter, and one special character.'
         );
         return;
       }
-  
+
       // Clear any existing errors
       setError('');
-      
-      try {
-        const[data, error] = await supabase.from('users').insert([{email, password}]);
 
-        if(error) {
-          console.error('There was an error: ', error.message)
+      try {
+        const [data, error] = await supabase.from('users').insert([{ email, password }]);
+
+        if (error) {
+          console.error('There was an error: ', error.message);
           setError('There was an error signing up, Try again.');
         } else {
           console.log('Data inserted successfully: ', data);
         }
-        } catch (error) {
-          console.error('Error: ', error.message);
-          setError('Error with sign up. Try aina.');
-        }
+      } catch (error) {
+        console.error('Error: ', error.message);
+        setError('Error with sign up. Try aina.');
       }
+    }
   
       // Proceed with sign-in logic
     };
@@ -99,6 +97,5 @@ const SignUp =() =>{
       </div>
       
   );
-};
 
 export default SignUp;
