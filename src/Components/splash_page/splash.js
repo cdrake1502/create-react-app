48ee834a3efda841c1fc2ed6e9910ac82bc14b62
elifew <157037792+elifew@users.noreply.github.com>
2024-02-14 13:40:19 -0500

    Update splash.js

--- src/Components/splash_page/splash.js
+++ src/Components/splash_page/splash.js
@@ -6,32 +6,9 @@ import Logo from "./logo.png";
 
 
 const Splash = () =>{
-
-  // const navigateToSignUp = () => {
-  //   window.location.href = '/signup'
-  // };
-  // const navigateToLogin = () => {
-  //   window.location.href = '/login'
-  // };
-  // const navigateToNotePage = () => {
-  //   window.location.href = "/notepage"
-  // }
-  
-
   return(
   <body className="body">
-    // <div class="temp-splash">
-
-    //       // <div className="container">
-    //       //   <div className="top-left-box">
-    //       //     <img class="rectangle-temp-icon" src={Logo} alt="Logo"/>
-
-              
-             
-    //       //   </div>
-    //       // </div>
-      
-    // </div>
+    
     <div className="wrapper">
             <img className="icon" alt="" src={Logo} />
           </div>
