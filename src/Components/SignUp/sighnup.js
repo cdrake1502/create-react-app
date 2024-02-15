9c727cf83abc7c2b488b9eb4cd98212fd3c7f13a
elifew <elifew@users.noreply.github.com>
2024-02-15 11:31:43 -0500

    Update sighnup.js

--- src/Components/SignUp/sighnup.js
+++ src/Components/SignUp/sighnup.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './signup_global.css';
 import './signup_index.css';
+import { useNavigate } from 'react-router-dom';
 import {createClient} from '@supabase/supabase-js';
 const supabase = createClient('https://vyvojvrtkryvbsmcgzrq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzY5NzM2NiwiZXhwIjoyMDIzMjczMzY2fQ.PzXtntpiXdhHH0lMh0EgPLFU1sYm4piufRkM6k2fkq4');
 
