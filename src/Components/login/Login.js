3143a6ee8ed29b60f341559acbbe8d17f65f5a56
elifew <elifew@users.noreply.github.com>
2024-02-15 09:45:32 -0500

    Update Login.js

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -3,6 +3,8 @@ import {useNavigate} from 'react-router-dom';
 import './login_global.css';
 import './login_index.css';
 import Logo from './logo.png';
+import {createClient} from '@supabase/supabase-js';
+const supabase = createClient('https://vyvojvrtkryvbsmcgzrq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzY5NzM2NiwiZXhwIjoyMDIzMjczMzY2fQ.PzXtntpiXdhHH0lMh0EgPLFU1sYm4piufRkM6k2fkq4');
 
 
 const Login = async () => {
