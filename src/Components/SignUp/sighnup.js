3e9c3337be22f287e59f96d11a4a9ce3dd429779
elifew <elifew@users.noreply.github.com>
2024-02-16 11:13:29 -0500

    Update sighnup.js

--- src/Components/SignUp/sighnup.js
+++ src/Components/SignUp/sighnup.js
@@ -15,6 +15,7 @@ const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
       e.preventDefault();
