0ca8b63a0a3673ae3c172228e763674b4001a9c6
Christian Drake <cdrake1502@gmail.com>
2024-04-08 02:02:41 -0400

    ugh

--- src/Components/authenticate/setLoginState.js
+++ src/Components/authenticate/setLoginState.js
@@ -1,7 +1,7 @@
 
 export function setLoginState(isLoggedIn, user, user_id) {
     sessionStorage.setItem('isLoggedIn', isLoggedIn);
-    sessionStorage.setItem('user-id', user_id);
+    sessionStorage.setItem('user_id', user_id);
     console.log(user_id);
     if (isLoggedIn) {
       sessionStorage.setItem('user', JSON.stringify(user));
