d136f66b332b163d38c38ec320b0728d881354cb
Christian Drake <cdrake1502@gmail.com>
2024-04-09 11:54:52 -0400

    Update saveQuiz.js

--- src/Components/savedQuizzes/saveQuiz.js
+++ src/Components/savedQuizzes/saveQuiz.js
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie'; // Assuming you've installed js-cookie
-
+// this probably wont work 
 export function saveQuizData(quizName, quizString) {
   const stringifiedData = JSON.stringify(quizString);
   const expirationDays = 30; // Adjust expiration time as needed (e.g., 7 days)
