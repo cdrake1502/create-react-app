1ab71eeaa056c5ca0ba13b1d205d8e81d9429c2e
Christian Drake <cdrake1502@gmail.com>
2024-04-09 21:31:23 -0400

    test

--- src/Components/displayQuizzes/displayQuizzes.js
+++ src/Components/displayQuizzes/displayQuizzes.js
@@ -4,7 +4,10 @@ import { createClient} from '@supabase/supabase-js';
 import {getQuizzes} from '../apiresponse/getQuizzes';
 
 
-const supabase = createClient('https://vyvojvrtkryvbsmcgzrq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzY5NzM2NiwiZXhwIjoyMDIzMjczMzY2fQ.PzXtntpiXdhHH0lMh0EgPLFU1sYm4piufRkM6k2fkq4');
+
+const supabaseUrl = 'https://vyvojvrtkryvbsmcgzrq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dm9qdnJ0a3J5dmJzbWNnenJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2OTczNjYsImV4cCI6MjAyMzI3MzM2Nn0.g0vq8MRsOox8wKVEq8gde9CA_2egvpfMfoqDHiWJxv4';
+const supabase = createClient(supabaseUrl, supabaseKey); 
 
 
 const DisplayQuiz = () => {
